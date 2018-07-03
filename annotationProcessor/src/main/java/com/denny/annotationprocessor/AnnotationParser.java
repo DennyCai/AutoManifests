a66528d9a714b4b3dbe9339ffc441f6a3c7f7fb3
dennycai <545965765@qq.com>
2018-07-03 19:41:51 +0800

    fix merge name

--- annotationProcessor/src/main/java/com/denny/annotationprocessor/AnnotationParser.java
+++ annotationProcessor/src/main/java/com/denny/annotationprocessor/AnnotationParser.java
@@ -11,8 +11,12 @@ import org.dom4j.tree.DefaultAttribute;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
@@ -35,9 +39,77 @@ public class AnnotationParser {
 
     public org.dom4j.Element parse(javax.lang.model.element.Element clzEle, Class<? extends Annotation> annClass) {
         Annotation annotation = clzEle.getAnnotation(annClass);
-        checkType(clzEle, annotation, annClass);
-        List<KeyValue> kvList = parseAnnotation(clzEle, annotation, annClass, mValidator);
+        System.out.println(clzEle.toString() + " ignore:" + ignore(annotation, annClass));
+        if (ignore(annotation, annClass)) {
+            return null;
+        }
+        Deque<TypeElement> extendHierarchy = checkType(clzEle, annotation, annClass);
+        System.out.println(extendHierarchy.toString());
+        Deque<Map<String, String>> keyValues = parseHierarchyAnnotation(extendHierarchy, annClass, mValidator);
+        System.out.println(keyValues);
+        List<KeyValue> kvList = mergeHierarchyKeyValues(keyValues, mValidator);
+        System.out.println("merge:" + kvList);
         return transformElement(annClass.getSimpleName(), kvList);
+    }
+
+    private boolean ignore(Annotation annotation, Class<? extends Annotation> annClass) {
+        try {
+            Method ignore = annClass.getDeclaredMethod("ignore", null);
+            return (boolean) ignore.invoke(annotation, null);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Deque<Map<String, String>> parseHierarchyAnnotation(Deque<TypeElement> extendHierachy, Class<? extends Annotation> annClass, Validator validator) {
+        TypeElement type = null;
+        Deque<Map<String, String>> deque = new LinkedList<>();
+        while (!extendHierachy.isEmpty()) {
+            type = extendHierachy.pop();
+            Map<String, String> result = parseAnnotation(type, type.getAnnotation(annClass), annClass, mValidator);
+            if (!result.isEmpty()) {
+                deque.add(result);
+            }
+        }
+        return deque;
+    }
+
+    private List<KeyValue> mergeHierarchyKeyValues(Deque<Map<String, String>> keyVals, Validator validator) {
+        Map<String, String> base = keyVals.pollLast();
+        Map<String, String> willMerge = null;
+        while (!keyVals.isEmpty()) {
+            willMerge = keyVals.pollLast();
+            base.put("name", willMerge.remove("name"));//replace name attr
+
+            for (Map.Entry<String, String> entry : willMerge.entrySet()) {
+                String val;
+                String key = entry.getKey();
+                String baseVal = base.get(key);
+                String newVal = entry.getValue();
+                if (baseVal != null) {
+                    val = baseVal;
+                    String firstSym = newVal.substring(0, 1);
+                    switch (firstSym) {
+                        case "+":
+                            val += newVal;
+                            break;
+                        default:
+                            val = newVal;
+                    }
+                } else {
+                    val = newVal;
+                }
+                // TODO: 2018/7/2 need validate after value merged
+                base.put(key, val);
+            }
+        }
+
+
+        List<KeyValue> list = new LinkedList<>();
+        for (Map.Entry<String, String> entry : base.entrySet()) {
+            list.add(new KeyValue(entry.getKey(), entry.getValue()));
+        }
+        return list;
     }
 
     private org.dom4j.Element transformElement(String elementName, List<KeyValue> kvList) {
@@ -48,10 +120,13 @@ public class AnnotationParser {
         return element;
     }
 
-    private List<KeyValue> parseAnnotation(Element clzEle, Annotation annotation, Class<? extends Annotation> annClass, Validator validator) {
+    private Map<String, String> parseAnnotation(Element clzEle, Annotation annotation, Class<? extends Annotation> annClass, Validator validator) {
+        if (annotation == null) {
+            return Collections.emptyMap();
+        }
         Method[] methods = annClass.getDeclaredMethods();
-        List<KeyValue> keyValues = new LinkedList<>();
-        keyValues.add(new KeyValue("name", clzEle.toString()));
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name", clzEle.toString());
         for (Method method : methods) {
             String key = method.getName();
             String val = null;
@@ -59,13 +134,13 @@ public class AnnotationParser {
                 String res = method.invoke(annotation, null).toString();
                 val = validateValue(res, clzEle, method, method.getAnnotations(), validator);
                 if (!StringUtils.isEmpty(val)) {
-                    keyValues.add(new KeyValue(key, val));
+                    map.put(key, val);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        return keyValues;
+        return map;
     }
 
     private String validateValue(String val, Element comEle, Method method, Annotation[] annotations, Validator validator) {
@@ -76,9 +151,9 @@ public class AnnotationParser {
     }
 
 
-    protected void checkType(Element clzEle, Annotation annotation, Class<? extends Annotation> annClass) {
+    protected Deque<TypeElement> checkType(Element clzEle, Annotation annotation, Class<? extends Annotation> annClass) {
         TypeElement type = mEleUtils.getTypeElement(clzEle.toString());
-        mChecker.checkExtendsFrom(type, annClass);
+        return mChecker.checkExtendsFrom(type, annClass);
     }
 
     public org.dom4j.Element addMainInterFilter() {
