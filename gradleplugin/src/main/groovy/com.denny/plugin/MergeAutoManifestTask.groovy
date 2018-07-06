19c7c39da64bddb7e063fe97053be9ea292f37f5
dennycai <545965765@qq.com>
2018-07-06 20:16:05 +0800

    update

--- gradleplugin/src/main/groovy/com.denny/plugin/MergeAutoManifestTask.groovy
+++ gradleplugin/src/main/groovy/com.denny/plugin/MergeAutoManifestTask.groovy
@@ -3,6 +3,7 @@ package com.denny.plugin
 import com.android.build.gradle.internal.LoggerWrapper
 import com.android.manifmerger.ManifestMerger2
 import com.android.manifmerger.MergingReport
+import org.apache.commons.io.FileUtils
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
@@ -15,13 +16,49 @@ class MergeAutoManifestTask extends DefaultTask {
 
     @TaskAction
     def mergeAutoManifest() {
-        def result = ManifestMerger2.newMerger(sourceManifest, new LoggerWrapper(project.logger), ManifestMerger2.MergeType.APPLICATION)
-                .addLibraryManifest(autoManifest)
-                .setMergeReportFile(reportFile)
-                .withFeatures(ManifestMerger2.Invoker.Feature.NO_IMPLICIT_PERMISSION_ADDITION)
-                .merge()
-        def str = result.getMergedDocument(MergingReport.MergedManifestKind.MERGED)
-        outManifest.write(str)
+        def result = null
+        try {
+            result = ManifestMerger2.newMerger(sourceManifest, new LoggerWrapper(project.logger), ManifestMerger2.MergeType.APPLICATION)
+                    .addLibraryManifest(autoManifest)
+                    .setMergeReportFile(reportFile)
+                    .withFeatures(ManifestMerger2.Invoker.Feature.NO_IMPLICIT_PERMISSION_ADDITION)
+                    .merge()
+            def str = result.getMergedDocument(MergingReport.MergedManifestKind.MERGED)
+            backupManifest(outManifest)
+            outManifest.write(str)
+        } catch (Exception e) {
+            e.printStackTrace()
+        }
+        if (result!= null && result.result.error) {
+            result.loggingRecords.each {
+                it.message.println(System.err)
+            }
+            result.intermediaryStages.each {
+                it.println(System.err)
+            }
+            throw new IllegalStateException()
+        }
+
+
+//        def xml = new XmlParser().parse(outManifest)
+//        Node app = xml.application[0]
+//        app.appendNode('meta-data', ['test':'test'])
+//
+//        outManifest.withPrintWriter {
+//            def printer = new XmlNodePrinter(it)
+//            printer.preserveWhitespace = true
+//            printer.print(xml)
+//        }
+
+    }
+
+    def backupManifest(File src) {
+        if (src!= null && src.exists()) {
+            File bk = new File(src.parentFile, "${src.name}.bk")
+            if (!bk.exists()) {
+                FileUtils.copyFile(src, bk)
+            }
+        }
     }
 
 }
